c64e7e2c324961e3bc81b9462a12442fbb2d3a08
Rohan0004 <110601451+Rohan0004@users.noreply.github.com>
2024-12-29 20:40:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
  function calculateArea() {
             // Get the radius from the user using the prompt() function
-            let radius = prompt("Enter the radius of the circle:");
+             let radius = parseInt(prompt("Enter the radius of the circle:"));
             // write you code here and display the result to the user
+	 if(Number.isInteger(radius) && radius > 0){
+		 alert((Math.PI * Math.pow(radius, 2)).toFixed(2));
+		
+	 }else {
+    alert("Please enter a positive integer for radius.");
+}
            
 }
 calculateArea();
