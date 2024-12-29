00e3592344d5b9aa26529780d7dd00001b98b80c
Rohan0004 <110601451+Rohan0004@users.noreply.github.com>
2024-12-29 20:45:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
              let radius = parseInt(prompt("Enter the radius of the circle:"));
             // write you code here and display the result to the user
 	 if(Number.isInteger(radius) && radius > 0){
-		 alert((Math.PI * Math.pow(radius, 2)).toFixed(2));
+		 let ans = (Math.PI * Math.pow(radius, 2)).toFixed(2);
+		 alert(`The area of the circle with radius ${radius} is ${ans}`);
 		
 	 }else {
     alert("Please enter a positive integer for radius.");
